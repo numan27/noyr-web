5350fba15b545356d7562f0891385f23d194cf6d
Numan Khalil <numankhalil27@gmail.com>
2025-05-15 00:24:15 +0500

    Meta Pixel added

--- app/layout.tsx
+++ app/layout.tsx
@@ -76,8 +76,37 @@ export default function RootLayout({
       <head>
         <link rel="icon" href="/icon.png" />
         <link rel="apple-touch-icon" href="/icon.png" />
+
+        {/* Meta Pixel Code */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '2730615080467623');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
       </head>
       <body>
+        {/* Meta Pixel NoScript Fallback */}
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=2730615080467623&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
+
         <CustomProvider>
           {children}
           <WhatsAppCTA />
