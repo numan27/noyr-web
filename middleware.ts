f09df261cc0fdbd0c6f7b8be57ef52bf4f487227
Numan Khalil <numankhalil27@gmail.com>
2025-05-21 12:23:05 +0500

    WhatsApp number updated

--- middleware.ts
+++ middleware.ts
@@ -1,43 +1,43 @@
-// import { redirectBasedOnRole } from "utils/server-side-helper";
-// import type { NextRequest } from "next/server";
-// import { NextResponse } from "next/server";
-// export function middleware(request: NextRequest) {
-//   let hasAccess: any = redirectBasedOnRole(request, request.nextUrl.pathname);
-
-//   if (!hasAccess.isAutherized && hasAccess.path !== request.nextUrl.pathname) {
-//     return NextResponse.redirect(new URL(hasAccess.path, request.url));
-//   }
-//   return NextResponse.next();
-// }
-
-// export const config = {
-//   matcher: ["/"],
-// };
-
-// Maintenance Mode
-
 import { redirectBasedOnRole } from "utils/server-side-helper";
 import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
-
 export function middleware(request: NextRequest) {
-  // ✅ Toggle maintenance mode
-  const maintenance = true;
-
-  // ✅ Bypass redirect if already on /maintenance
-  if (maintenance && !request.nextUrl.pathname.startsWith("/maintenance")) {
-    return NextResponse.rewrite(new URL("/maintenance", request.url));
-  }
-
-  // Your existing role-based logic
   let hasAccess: any = redirectBasedOnRole(request, request.nextUrl.pathname);
+
   if (!hasAccess.isAutherized && hasAccess.path !== request.nextUrl.pathname) {
     return NextResponse.redirect(new URL(hasAccess.path, request.url));
   }
-
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/((?!_next|favicon.ico|images|icons|fonts|maintenance).*)"],
+  matcher: ["/"],
 };
+
+// Maintenance Mode
+
+// import { redirectBasedOnRole } from "utils/server-side-helper";
+// import type { NextRequest } from "next/server";
+// import { NextResponse } from "next/server";
+
+// export function middleware(request: NextRequest) {
+//   // ✅ Toggle maintenance mode
+//   const maintenance = true;
+
+//   // ✅ Bypass redirect if already on /maintenance
+//   if (maintenance && !request.nextUrl.pathname.startsWith("/maintenance")) {
+//     return NextResponse.rewrite(new URL("/maintenance", request.url));
+//   }
+
+//   // Your existing role-based logic
+//   let hasAccess: any = redirectBasedOnRole(request, request.nextUrl.pathname);
+//   if (!hasAccess.isAutherized && hasAccess.path !== request.nextUrl.pathname) {
+//     return NextResponse.redirect(new URL(hasAccess.path, request.url));
+//   }
+
+//   return NextResponse.next();
+// }
+
+// export const config = {
+//   matcher: ["/((?!_next|favicon.ico|images|icons|fonts|maintenance).*)"],
+// };
